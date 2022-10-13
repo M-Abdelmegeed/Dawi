f89d61777cffe4d11fb6a694b5ebdba6ac9ac0ec
Your Name <you@example.com>
2022-10-13 13:02:00 +0200

    Updated CORS Policy

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ app.use(router);
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
-
-  app.use(cors());
+  const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.get('/', function (req, res) {
 res.send('Hello World');
