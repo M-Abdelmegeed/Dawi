71e035e4c0fdb1dd4f946a41a5cf09e303e9b38f
Your Name <you@example.com>
2022-10-13 15:59:01 +0200

    Updated CORS Policy

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 8081;
 app.use(express.json());
+app.use(cors({
+  origin:"https://e7gezli-frontapp.herokuapp.com"
+}));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(router);
@@ -16,10 +19,6 @@ app.use(router);
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
-  const corsOptions ={
-    origin:'https://e7gezli-frontapp.herokuapp.com', 
-}
-app.use(cors(corsOptions));
 
 app.get('/', function (req, res) {
 res.send('Hello World');
