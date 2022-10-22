df931f9edf759bfa608a136a225356365c4bce12
Your Name <you@example.com>
2022-10-22 22:33:40 +0200

    Updated CORS origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors({
-  origin:"https://e7gezli-frontapp.herokuapp.com"
+  origin:"https://agile-final.herokuapp.com/"
 }));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
