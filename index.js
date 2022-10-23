55d9786d48cd4275bb41f6c04c4fba4c798a1750
Your Name <you@example.com>
2022-10-23 15:49:45 +0200

    Updated CORS

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.use(cors({
 app.use(cors({
   origin:"https://agile-final.herokuapp.com"
 }));
+app.use(cors({
+  origin:"https://e7gezli-frontapp.herokuapp.com"
+}));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(router);
