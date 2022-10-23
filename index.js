b218cdbb3b41317c93c20fe84a517bbad1ba8a28
Your Name <you@example.com>
2022-10-23 15:56:00 +0200

    CORS

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ const PORT = process.env.PORT || 8080;
 // app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
-  origin:"https://agile-final.herokuapp.com/doctor-form"
-}));
-app.use(cors({
-  origin:"https://agile-final.herokuapp.com"
-}));
-app.use(cors({
-  origin:"https://e7gezli-frontapp.herokuapp.com"
+  origin:["https://agile-final.herokuapp.com/doctor-form","https://agile-final.herokuapp.com"]
 }));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }))
