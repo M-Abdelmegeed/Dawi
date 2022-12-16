1fcb296e3343dcc7019f84ee02688aaa8fc4a080
Mohammed <mohammed@outlook.com>
2022-12-16 21:37:46 +0200

    Updated CORS Policy

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 8080;
 // app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
-  origin:["https://agile-final.herokuapp.com/doctor-form","https://agile-final.herokuapp.com"]
+  origin:["https://agile-final.netlify.app/doctor-form","https://agile-final.netlify.app"
+  ,"https://agile-final.netlify.app/login,https://agile-final.netlify.app/home-page"]
 }));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }))
