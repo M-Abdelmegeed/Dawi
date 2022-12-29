5858d167e025bfd288949637afb90f0bc53a8030
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2022-12-29 18:11:41 +0200

    Updated editMedicine

--- src/controllers/editMedicine.js
+++ src/controllers/editMedicine.js
@@ -5,15 +5,17 @@ const editMedicine = async (req,res)=>{
     const name=req.body.name;
     const expiryDate=req.body.expiryDate;
     // const timestamp=new Date();
-    // const pharmacistID=req.user.id;
+    const pharmacistID=req.body.pharmacistID;
     const query={_id: medID};
     const med= await Medicine.findOne(query);
     console.log(med);
     if(med!=null){
         await Medicine.findOneAndUpdate(query,{$set: { name:name, expiryDate:expiryDate}});
-        res.send("Medicine updated successfully!");
+        const medicines = await Medicine.find({ pharmacistID: pharmacistID }).exec();
+        res.json(medicines);
+        // res.send("Medicine updated successfully!");
     }else{
-        res.send("Sorry an error occured, please try again.");
+        res.send("Sorry, please try again!");
     }
 }
 
